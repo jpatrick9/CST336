303d70304b8cafdc0ed2063e0831a410c9d03a81
joshpat9 <jpatrick@csumb.edu>
2018-02-25 08:05:19 +0000

    first commit

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -1,75 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Lab 2: 777 Slot Machine </title>
-        <meta charset="utf-8" />
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
         
-        <?php
-        
-        function displaySymbol($randomValue){
-           $randomValue = rand(0,2);
-           echo $randomValue;
-           
-           if ($randomValue == 0) {
-               $symbol = "seven";
-           } else if ($randomValue == 1) {
-               $symbol = "orange";
-           } else {
-               $symbol = "cherry";
-           }
-           
-           switch($randomValue){
-               case 0: $symbol = "seven";
-                            break;
-                case 1: $symbol = "orange";
-                            break;
-                 case 2: $symbol = "cherry";
-                            break;   
-           }
-           
-         echo "<img src='img/$symbol.png' width='70' alt=\"$symbol\" title=\"$symbol\" />";
+        <div id="main">
+            <!-- <img src="img/cherry.png" alt="cherry" title="Cherry" width="70"/> -->
+            <?php
             
-        }
-        
-        function displayPoints($randomValue1, $randomValue2, $randomValue3){
-            echo "<div id = 'output'>";
-            if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3){
-                switch ($randomValue1) {
-                    case 0:
-                        // code...
-                        break;
-                    
-                    default:
-                        // code...
-                        break;
-                }
-            }
-        }
-        
-        $randomValue1 = rand(0,2);
-        displaySymbol($randomValue1);
-        
-        $randomValue2 = rand(0,2);
-        displaySymbol($randomValue2);
-        
-        $randomValue3 = rand(0,2);
-        displaySymbol($randomValue3);
-        
-        echo "<br /> <hr> Values: $randomValue1 $randomValue2 $randomValue3"
-        
-        //for ($i=0; $i < 3; $i++) {
-        //       displaySymbol();
-        //    } 
-           
-        ?>
-
-<!--
-         <img src="img/lemon.png" width="70" alt="Lemon" title="Lemon" />
-         <img src="img/cherry.png" width="70" alt="Cherry" title="Cherry" />
-         <img src="img/orange.png" width="70" alt="Orange" title="Orange" />
--->
-
+            include 'inc/functions.php';
+            play();
+            
+            /*
+            $randomValue1 = rand(0,2);
+            displaySymbol($randomValue1);
+            $randomValue2 = rand(0,2);
+            displaySymbol($randomValue2);
+            $randomValue3 = rand(0,2);
+            displaySymbol($randomValue3);
+            */
+            ?>
+            
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
     </body>
 </html>
