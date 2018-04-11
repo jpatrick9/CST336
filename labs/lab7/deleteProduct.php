b2d396a9b6cad78c724beff11025df064e549ea2
joshpat9 <jpatrick@csumb.edu>
2018-04-11 03:58:08 +0000

    Added basic layout and buttons

--- labs/lab7/deleteProduct.php
+++ labs/lab7/deleteProduct.php
@@ -4,7 +4,7 @@
     
  $connection = getDatabaseConnection("ottermart");
     
- $sql = " DELETE FRMO om_product WHERE productId = " . $_GET['productId'];
+ $sql = " DELETE FROM om_product WHERE productId = " . $_GET['productId'];
  
  $statement = $connection->prepare($sql);
  $statement->execute();
